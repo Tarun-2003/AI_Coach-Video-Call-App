06f063c7991f7cc1693a5ab096496e8122e00621
Tarun-2003 <113117993+Tarun-2003@users.noreply.github.com>
2025-10-14 03:49:57 +0100

    011-Agents-Form-partially-completed

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -7,22 +7,24 @@ import { Suspense } from "react";
 import { LoadingState } from "@/components/loading-state";
 // import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import {ErrorBoundary} from "react-error-boundary";
+import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
+
 const Page = async () => {
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
 
+
   return (
+    <>
+   <AgentsListHeader />
     <HydrationBoundary state={dehydrate(queryClient)}>
       <Suspense fallback={<AgentsViewLoading/>}>
       <ErrorBoundary fallback={<AgentsViewError/>}>
           <AgentsView />
-
       </ErrorBoundary>
-           
-
       </Suspense>
- 
     </HydrationBoundary>
+    </>
   );
 };
 
--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,16 +1,45 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
-
-
+import { createTRPCRouter, baseProcedure, protectedProcedure } from "@/trpc/init";
+import { agentsInsertSchema } from "../schemas";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
 export const agentsRouter = createTRPCRouter({
+  // getOne: baseProcedure.query(async () => {
+  //   const data = await db
+  //     .select()
+  //     .from(agents);
+  //   return data;
+
+  // }), 
+    getOne: baseProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [existingAgent] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, input.id));
+
+      return existingAgent;
+    }),
+  
+  
   getMany: baseProcedure.query(async () => {
     const data = await db
       .select()
       .from(agents);
-
-    //   await new Promise((resolve) => setTimeout(resolve,5000));
-// throw new TRPCError({code:"BAD_REQUEST"});
     return data;
   }),
+  create:protectedProcedure.input(agentsInsertSchema)
+                           .mutation(async ({ input, ctx }) => {
+      const [createdAgent] = await db
+        .insert(agents)
+        .values({
+          ...input,
+          userId: ctx.auth.user.id,
+        })
+        .returning();
+
+      return createdAgent;
+    }),
 });
